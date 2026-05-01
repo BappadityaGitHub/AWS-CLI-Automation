e8cfa5d7cddffc536fee21a62f56aa9bf7b12c37
BappadityaGitHub <bappadityasahoo23@gmail.com>
2026-05-02 02:09:22 +0530

    variable

--- variablescrip.sh
+++ variablescrip.sh
@@ -1,26 +1,4 @@
 #!/bin/bash
-echo "analysing log files"
-echo "=================================" 
-echo -e"\n finding List of log files in updated in last 24 hours"
-
-#run find . -name "*.log" -mtime -1 # "." after find stands for current directory , in that Finding which files have been updated in last 1 day 
-find /c/Users/Me/AWS-CLI-Automation/Logs -name "*.log" -mtime -1   #### we will be providing absolute path for the script to work from anywhere after example /c/Users/Me/AWS-CLI-Automation/Logs  
-# imaging log file name application.log and system.log and i have to find error or fatal or crital logs 
-echo " searching error logs " 
-grep "ERROR" /c/Users/Me/AWS-CLI-Automation/Logs/application.log #to find ERROR files
-echo -e "\n searching number of error and fatal logs " 
-grep -c "ERROR" /c/Users/Me/AWS-CLI-Automation/Logs/application.log # finding number of ERROR files
-grep -c "FATAL" /c/Users/Me/AWS-CLI-Automation/Logs/application.log #finding number of FATAL files
-grep -c "FATAL" /c/Users/Me/AWS-CLI-Automation/Logs/system.log # finding number of FATAL files
-grep -c "CRITICAL" /c/Users/Me/AWS-CLI-Automation/Logs/system.log # finding number of critilal files 
-# if there are multple log files which i need to check daily i will run command that will only pull out chnages made in last 24 hours in those log files with out making me go through them all repeatedly daily 
-# in the current directory will run : find . -name "*.log" -mtime -1 
-# -mtime filters file based on modification time , -1 is last 24 hour time
-# ## "*.log" should be double inverted commas as its string
-# #### if we save all the previous commands in one script and run that will be shell script
-#touch analyse-logs.sh
-# grep -c "FATAL" apt.
-#
 # ######################
 # Variables
 # ######################
